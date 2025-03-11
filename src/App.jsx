6a201f78ab84728401630bb34deb0cee7efd9bc8
Milenaslv20 <milenaslvmedeiros@hotmail.com>
2025-03-11 14:39:39 -0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -37,29 +37,32 @@ function App() {
   };
 
   const faceMyDetect = () => {
-    setInterval(async () => {
+    const detectFaces = async () => {
       const detections = await faceapi
         .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
         .withFaceLandmarks()
         .withFaceExpressions();
-
+  
       // Clear the canvas before drawing new detections
       const canvas = canvasRef.current;
       const context = canvas.getContext('2d');
       context.clearRect(0, 0, canvas.width, canvas.height); // Clear the entire canvas
-
+  
       // DRAW ONLY FACE DETECTIONS (ONLY SQUARE AROUND THE FACE)
       faceapi.draw.drawDetections(canvasRef.current, detections);
-    }, 100);
+  
+      // Chama detectFaces novamente no próximo quadro
+      requestAnimationFrame(detectFaces);
+    };
+  
+    // Inicia a detecção assim que o componente for montado
+    detectFaces();
   };
 
   return (
     <div className="myapp">
-      <h1>Face Detection</h1>
-
       <div className="appvide">
         <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
-
         <canvas ref={canvasRef} width="640" height="480" className="appcanvas" />
       </div>
     </div>
